54677caaae3a99ce6bda8117571f0ad7b4c10fd7
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-05-13 12:23:36 +0300

    hw 3 phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { initialData } from 'data';
+import { ContactForm } from './ContantForm/ContactForm.jsx';
+import { Filter } from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const locStorageContacts = localStorage.getItem('contacts');
+    this.setState({
+      contacts: locStorageContacts
+        ? JSON.parse(locStorageContacts)
+        : initialData,
+    });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts)
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
+  deleteContact = delId => {
+    const newList = this.state.contacts.filter(({ id }) => id !== delId);
+    this.setState({ contacts: newList });
+  };
+
+  existRow = existName => alert(`Record ${existName} already exists`);
+
+  handleForm = row => {
+    if (this.state.contacts.find(({ name }) => name === row.name)) {
+      this.existRow(row.name);
+      return;
+    }
+    this.setState(prev => {
+      return {
+        contacts: [{ id: nanoid(), ...row }, ...prev.contacts],
+      };
+    });
+  };
+
+  handlerChangeFilter = e => this.setState({ filter: e.target.value });
+
+  render() {
+    const { filter, contacts } = this.state;
+    return (
+      <div>
+        <h1 style={{textAlign:"center"}}>Phonebook</h1>
+        <ContactForm onForm={this.handleForm} />
+
+        <h2 style={{ textAlign: "center" }}>Contacts</h2>
+        <Filter value={filter} handlerChangeFilter={this.handlerChangeFilter} />
+        <ContactList
+          filter={filter}
+          contacts={contacts}
+          onDelete={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+
---
+++ src/components/ContantForm/ContactForm.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import { Form, Label, Input, Button } from './ContactForm.styled';
+import PropTypes from 'prop-types';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handlerSubmit = e => {
+    e.preventDefault();
+    const { onForm } = this.props;
+    let { name, number } = this.state;
+    onForm({ name, number });
+    e.target.reset();
+    this.setState({ name: '', number: '' });
+  };
+
+  handlerChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <Form onSubmit={e => this.handlerSubmit(e)}>
+        <Label htmlFor="name">Name </Label>
+        <Input
+          autoComplete='off'
+          value={name}
+          id="name"
+          type="text"
+          name="name"
+          onChange={this.handlerChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+
+        <Label htmlFor="tel">Number </Label>
+        <Input
+          autoComplete='off'
+          value={number}
+          id="tel"
+          type="tel"
+          name="number"
+          onChange={this.handlerChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <br />
+        <Button type="submit">Add contact</Button>
+      </Form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onForm: PropTypes.func.isRequired,
+};
+
+
